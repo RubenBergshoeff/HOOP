34b93e78b85cb7dbec0b6d2db17aae44829c3acc
RubenBergshoeff <ruben.bergshoeff@home.nl>
2017-09-11 13:13:04 +0200

    Clean c++ project added

---
+++ HOOP_assignment01_lesrooster/HOOP_assignment01_lesrooster/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HOOP_assignment01_lesrooster
+//
+//  Created by Ruben Bergshoeff on 11/09/2017.
+//  Copyright © 2017 Ruben Bergshoeff. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
